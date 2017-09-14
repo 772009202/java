82b91f7fdd651e0c693739dfbfab50277da33fa3
YuChen <77200902@qq.com>
2017-09-14 17:06:25 +0800

    代理模式

---
+++ src/javamode/proxy/SayHelloProxy.java
@@ -0,0 +1,17 @@
+package javamode.proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * 动态代理模式
+ * Created by YuChen on 2017/9/14 0014
+ */
+public class SayHelloProxy implements InvocationHandler{
+
+	@Override
+	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+		return null;
+	}
+
+}
---
+++ src/javamode/proxy/Test.java
@@ -0,0 +1,7 @@
+package javamode.proxy;
+
+/**
+ * Created by YuChen on 2017/9/14 0014
+ */
+public class Test {
+}
